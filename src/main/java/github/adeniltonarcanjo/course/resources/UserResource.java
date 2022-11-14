a0c026cd68d67350a2205f7c58440845641f7caa
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-11-14 13:35:11 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/github/adeniltonarcanjo/course/resources/UserResource.java
+++ src/main/java/github/adeniltonarcanjo/course/resources/UserResource.java
@@ -2,6 +2,7 @@ package github.adeniltonarcanjo.course.resources;
 
 
 import github.adeniltonarcanjo.course.entities.User;
+import github.adeniltonarcanjo.course.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -12,11 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    ResponseEntity<User> getUsers() {
-        User user = new User(null, "maria", "maria@gmail.com", "4723846732", "37829749324");
-        return ResponseEntity.ok().body(user);
-    }
+    UserRepository repository;
+
+
+
 
 
 }
