1c7a8d2be114f138aafbce57af639a10de1baf5f
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-11-14 15:41:13 -0300

    Order, Instant, ISO 8601 - Part two

---
+++ src/main/java/github/adeniltonarcanjo/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package github.adeniltonarcanjo.course.repositories;
+
+import github.adeniltonarcanjo.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/github/adeniltonarcanjo/course/resources/OrderResource.java
@@ -0,0 +1,34 @@
+package github.adeniltonarcanjo.course.resources;
+
+import github.adeniltonarcanjo.course.entities.Order;
+import github.adeniltonarcanjo.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+    @Autowired
+    private OrderService service;
+
+
+    @RequestMapping
+    ResponseEntity<List<Order>> findAll() {
+        List<Order> obj = service.findAll();
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = {"/{id}"})
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        Order obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
