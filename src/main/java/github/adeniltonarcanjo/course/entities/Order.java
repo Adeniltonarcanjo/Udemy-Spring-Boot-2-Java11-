860b52db5988907389278167c5670adfc9a503b8
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-11-16 20:29:23 -0300

    OrderStatus enum

--- src/main/java/github/adeniltonarcanjo/course/entities/Order.java
+++ src/main/java/github/adeniltonarcanjo/course/entities/Order.java
@@ -1,6 +1,7 @@
 package github.adeniltonarcanjo.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import github.adeniltonarcanjo.course.entities.enums.OrderStatus;
 import org.springframework.beans.factory.annotation.Configurable;
 
 import javax.persistence.*;
@@ -24,13 +25,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
